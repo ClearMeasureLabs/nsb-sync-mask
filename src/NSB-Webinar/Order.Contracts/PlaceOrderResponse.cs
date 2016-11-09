16fb9b92cfe2d71112c38068765bd8e040d1bd22
Justin Self <justinself@outlook.com>
2016-11-09 15:26:33 -0700

    added an example of an async message wrapped in a sync call

---
+++ src/NSB-Webinar/Order.Contracts/PlaceOrderResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using NServiceBus;
+
+namespace Order.Contracts
+{
+    public class PlaceOrderResponse : IMessage
+    {
+        public Guid OrderId { get; set; }
+    }
+}
--- src/NSB-Webinar/Order.UI/Controllers/HomeController.cs
+++ src/NSB-Webinar/Order.UI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using NServiceBus;
 using Order.Contracts;
@@ -7,7 +8,7 @@ using Order.UI.Models;
 
 namespace Order.UI.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : AsyncController
     {
         private readonly IBus _bus;
 
@@ -22,13 +23,14 @@ namespace Order.UI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Order(string name)
+        public async Task<ActionResult> Order(string name)
         {
-            var orderId = Guid.NewGuid();
-
-            _bus.Send(new PlaceOrder {OrderId = orderId, CustomerId = name});
-            //Thread.Sleep(3000);
-            return Json(new OrderModel { Name = name, OrderId = orderId });
+            await _bus.Send(new PlaceOrder { OrderId = Guid.NewGuid(), CustomerId = name })
+                .Register(completion =>
+                {
+                    AsyncManager.Parameters["guid"] = ((PlaceOrderResponse) completion.Messages[0]).OrderId;
+                });
+            return Json(new OrderModel { Name = name, OrderId = Guid.Parse(AsyncManager.Parameters["guid"].ToString()) });
         }
     }
 }
